1f4fd1e1604dff43b9efeb6f5f1555a4fdca8215
RodwelJunior09 <me@Mes-MacBook-Pro.local>
2020-05-17 13:41:00 -0400

    Movement of the ship, on the x asis and y asis

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaY = Input.GetAxis("Vertical");
+        var deltaX = Input.GetAxis("Horizontal");
+        var newXpos = transform.position.x + deltaX;
+        var newYpos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXpos, newYpos);
+    }
+}
