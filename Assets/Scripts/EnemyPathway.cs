fdf51e7a8791c70acb03836662916664acdc5f20
RodwelJunior09 <rodweljunior@gmail.com>
2020-05-25 12:20:25 -0400

    Enemy Spawning and Wave Configuration Files

---
+++ Assets/Scripts/EnemyPathway.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathway : MonoBehaviour
+{
+    // Config Variables
+    [SerializeField] private WaveConfig waveConfig;
+    [SerializeField] private float moveSpeed = 2f;
+
+    private int wayPointIndex = 0;
+    private List<Transform> _wayPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _wayPoints = waveConfig.GetWayPoints();
+        transform.position = _wayPoints[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementOfEnemy();
+    }
+
+    private void MovementOfEnemy()
+    {
+        if (wayPointIndex <= _wayPoints.Count - 1)
+        {
+            var target = _wayPoints[wayPointIndex].transform.position;
+            var movement = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target, movement);
+            if (transform.position == target)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,18 @@ public class Player : MonoBehaviour
 {
     // Config Params
     [SerializeField] float speedOfPlayer = 10f;
-    [SerializeField] float padding = 1f;
-    [SerializeField] GameObject playerLaser;
+    [SerializeField] float _padding = 1f;
+    [SerializeField] private GameObject _playerLaser;
     [SerializeField] private float projectileSpeed = 1f;
     [SerializeField] private float projectFirePeriod = 1f;
 
-    private Coroutine firingCourutine;
+    private Coroutine _fireCoroutine;
 
     // Local Variables
-    private float xMin;
-    private float xMax;
-    private float yMin;
-    private float yMax;
+    private float _xMin;
+    private float _xMax;
+    private float _yMin;
+    private float _yMax;
 
     // Start is called before the first frame update
     void Start()
@@ -31,16 +31,15 @@ public class Player : MonoBehaviour
         Fire();
     }
 
-
     private void Fire()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            firingCourutine = StartCoroutine(ContinousFire());
+            _fireCoroutine = StartCoroutine(ContinousFire());
         }
         if (Input.GetButtonUp("Fire1"))
         {
-            StopCoroutine(firingCourutine);   
+            StopCoroutine(_fireCoroutine);   
         }
     }
 
@@ -48,7 +47,7 @@ public class Player : MonoBehaviour
     {
         while (true)
         {
-            var laser = Instantiate(playerLaser, transform.position, Quaternion.identity) as GameObject;
+            var laser = Instantiate(_playerLaser, transform.position, Quaternion.identity) as GameObject;
             laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
             yield return new WaitForSeconds(projectFirePeriod);
         }
@@ -58,18 +57,18 @@ public class Player : MonoBehaviour
     {
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speedOfPlayer;
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speedOfPlayer;
-        var newXpos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
-        var newYpos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        var newXpos = Mathf.Clamp(transform.position.x + deltaX, _xMin, _xMax);
+        var newYpos = Mathf.Clamp(transform.position.y + deltaY, _yMin, _yMax);
         transform.position = new Vector2(newXpos, newYpos);
     }
 
     private void SetupMoveBoundaries()
     {
         Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+        _xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + _padding;
+        _xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - _padding;
+        _yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + _padding;
+        _yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - _padding;
     }
 
 }
