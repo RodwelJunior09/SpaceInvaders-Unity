ba8f0f07e49a66dedd721079a09430338b609dcb
RodwelJunior09 <rodweljunior@gmail.com>
2020-05-30 18:00:06 -0400

    New Features, Music Added, New Enemy PathWay, New Scenes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Net;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    [Header("Enemy Stats")]
     [SerializeField] private float health = 800;
     [SerializeField] private float shotCounter;
     [SerializeField] private float minBetweenShots = 0.2f;
     [SerializeField] private float maxBetweenShots = 3f;
-
-    [SerializeField] private GameObject explosionGameObject;
-    [SerializeField] private GameObject enemyLaserPrefab;
     [SerializeField] private float projectileSpeed;
     [SerializeField] private float durationExplosion = 1f;
+
+    [Header("Enemy Object Components")]
+    [SerializeField] private GameObject explosionGameObject;
+    [SerializeField] private GameObject enemyLaserPrefab;
+
+    [Header("Enemy Sounds")] 
+    [SerializeField] private AudioClip _laserAudioClip;
+    [SerializeField] private AudioClip _explosionAudioClip;
+
+    [Header("Volume Adjustment")]
+    [SerializeField, Range(1f, 10f)] private float _laserVolume = 2f;
+    [SerializeField, Range(1f, 10f)] private float _explosionVolume = 2f;
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +53,7 @@ public class Enemy : MonoBehaviour
     {
         var laser = Instantiate(enemyLaserPrefab, transform.position, Quaternion.identity);
         laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        AudioSource.PlayClipAtPoint(_laserAudioClip, Camera.main.transform.position, _laserVolume);
     }
 
     private void OnTriggerEnter2D(Collider2D otherCollision)
@@ -73,6 +80,7 @@ public class Enemy : MonoBehaviour
     {
         Destroy(gameObject);
         GameObject vfxExplosion = Instantiate(explosionGameObject, transform.position, transform.rotation);
+        AudioSource.PlayClipAtPoint(_explosionAudioClip, Camera.main.transform.position, _explosionVolume);
         Destroy(vfxExplosion, durationExplosion);
     }
 }
